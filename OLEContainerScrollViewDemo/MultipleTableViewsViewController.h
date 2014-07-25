33fb96bf91b733ecae49e824a42622bb23b4b712
Ole Begemann <ole@oleb.net>
2014-07-26 00:16:54 +0200

    Add multiple table views scene to demo app

---
+++ OLEContainerScrollViewDemo/MultipleTableViewsViewController.h
@@ -0,0 +1,12 @@
+/*
+ OLEContainerScrollView
+ 
+ Copyright (c) 2014 Ole Begemann.
+ https://github.com/ole/OLEContainerScrollView
+ */
+
+@import UIKit;
+
+@interface MultipleTableViewsViewController : UIViewController
+
+@end
